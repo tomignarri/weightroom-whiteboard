7f565fe468467e27e13060e435f99b071a3a4dea
tomignarri <tom.ignarri@gmail.com>
2025-03-04 09:04:31 -0500

    Added 2fa signup form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,14 @@
+import { useState } from "react"
+
 export default function LoginForm() {
     return (
       <div>
         <h1>Login</h1>
-        <label>Email</label>
-        <input type="email" />
+        <label>Enter Code</label>
+        <input type="text" />
         
+        <label>Submit</label>
+        <input type="submit" />
         
       </div>
     )
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,15 @@ export default function SignUp() {
     return (
       <div>
         <h1>Sign up</h1>
+
+        <label>Email</label>
+        <input type="text" />
+
+        <label>Phone Number</label>
+        <input type="text" />
+
+        <label>Submit</label>
+        <input type="submit" />
         
       </div>
     )
